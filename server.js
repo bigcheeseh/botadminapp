4ff7609209c475521d7ca878ce81f980e8055676
user.email <bigcheeseh@gmail.com>
2017-11-05 03:03:31 +0300

    server settings

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 
 const keys = require('./config/keys');
 const app = express();
